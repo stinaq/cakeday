5766e0a92696e2fb76992713df2462ac4b093870
Stina Qvarnström <stinaqv@gmail.com>
2013-02-20 15:47:54 +0100

    adds a tiny version of the site

---
+++ tinyversion/app.js
@@ -0,0 +1,29 @@
+$(function(){
+
+	$('#cakedayForm').on("submit", function(event) {
+		event.preventDefault();
+		var user = $("#userName").val();
+		displayDaysLeft(user);
+	});
+
+	var displayDaysLeft = function(user) {
+		user = $("#userName").val();
+		var userUrl = "http://www.reddit.com/user/" + user + "/about.json?jsonp=?";
+
+		$.getJSON(userUrl, function(data){
+ 	 		var createdDate = moment.unix(data.data.created);
+			var daysLeft = caluclateDaysLeft(createdDate);
+			$('#result').text(daysLeft);
+		});
+	};
+
+	var caluclateDaysLeft = function(createdDate) {
+		var currentYear = moment().format('YYYY');
+		var nextCakeday = createdDate.year(currentYear);
+
+		if(nextCakeday < moment()){
+			nextCakeday = nextCakeday.add('years', 1)
+		}
+		return nextCakeday.diff(moment(), 'days');
+	};
+});
